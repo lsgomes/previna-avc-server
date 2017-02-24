d12d4ff94c242d6fe38322d17289b1c0984c7640
lsgomes <lucas.gomes.canoas@gmail.com>
2017-02-24 01:25:21 -0300

    Add list properties, individuals

--- app/src/main/scala/obj.scala
+++ app/src/main/scala/obj.scala
@@ -7,8 +7,12 @@
 import org.apache.jena.rdf.model.ModelFactory
 import org.apache.jena.ontology.{OntClass, OntModel, OntModelSpec}
 import org.apache.jena.util.FileManager
+import org.apache.jena.vocabulary.VCARD
+import org.apache.jena.ontology.OntDocumentManager
 
 object obj extends App {
+
+  val namespace = "http://www.semanticweb.org/lucas/ontologies/2016/9/stroke#"
 
   val personURI = "http://somewhere/JohnSmith"
   val fullName = "John Smith"
@@ -19,15 +23,61 @@ object obj extends App {
 
   //johnSmith.addProperty()
 
-  val model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM)
+  val ontologyDocumentManager = new OntDocumentManager()
+
+  ontologyDocumentManager.setProcessImports(true)
+
+  val ontologyModelSpec = new OntModelSpec( OntModelSpec.OWL_MEM_TRANS_INF )
+
+  ontologyModelSpec.setDocumentManager(ontologyDocumentManager)
+
+  val modelOntology = ModelFactory.createOntologyModel(ontologyModelSpec)
+
+
+  val model = ModelFactory.createDefaultModel()
 
   val in = FileManager.get().open("stroke_v2.owl")
 
-  model.read(in, null)
+  //model.read(in, null)
 
-  val classList = model.listClasses().toList
+  modelOntology.read(in, null)
 
-  classList.forEach(println)
+
+  val lucas = model.createResource("lucas")
+
+  //modelOntology.write(System.out)
+
+
+
+  //modelOntology.listAllOntProperties().toList.forEach(println)
+
+  //val person =  modelOntology.getOntClass(namespace + "Person")
+
+  //person.listDeclaredProperties().toList.forEach(p => println(p.getLocalName))
+
+  val resourcePerson = modelOntology.getResource( namespace + "Person" )
+
+  modelOntology.listIndividuals().toList.forEach(println)
+  modelOntology.imp
+  val personLucas = modelOntology.createIndividual("Person Lucas", resourcePerson)
+
+  val hasAge = modelOntology.getProperty(namespace + "hasAge")
+
+  //val age = personLucas.addProperty(hasAge, "26")
+
+  println(personLucas.getProperty(hasAge))
+  //xprintln(personLucas.getProperty(hasSex))
+
+  personLucas.listOntClasses(true).toList.forEach(println)
+  println(personLucas.hasProperty(hasAge))
+
+  //modelOntology.listIndividuals().toList.forEach(i => i.getOntClass.listDeclaredProperties().toList.forEach(println))
+
+
+ // val classList = model.listClasses().toList
+
+  //classList.forEach(println)
+
 
 
 //  final Query query = QueryFactory.create("PREFIX rdf:<http://www.w3.org/2000/01/rdf-schema#> SELECT distinct ?class WHERE {?class a rdf:Class.}");
